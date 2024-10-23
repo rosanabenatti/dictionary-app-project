54726e2137e26e4847f4e904383583c84d6dab1b
R.Benatti <rosanabenatti@gmail.com>
2024-10-23 12:10:33 +0200

    Created Synonym component and coded.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -9,13 +9,24 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
+              <strong>Definition:</strong> {definition.definition}
             </p>
+            {definition.example && (
+              <p>
+                <strong>Example:</strong> <em>{definition.example}</em>
+              </p>
+            )}
           </div>
         );
       })}
+
+      {/* Now render synonyms from props.meaning.synonyms */}
+      {props.meaning.synonyms && props.meaning.synonyms.length > 0 && (
+        <div>
+          <strong>Synonyms:</strong>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </div>
+      )}
     </div>
   );
 }
